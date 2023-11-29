42b7d412287ac708676e873b02da4829f13ff2fc
xshenshirley <78037066+xshenshirley@users.noreply.github.com>
2023-11-29 09:47:16 +0000

    To complete Exercise 1

--- src/main/java/org/advent/day1/Elf.java
+++ src/main/java/org/advent/day1/Elf.java
@@ -15,10 +15,14 @@ public class Elf implements Comparable<Elf> {
         return totalCalories;
     }
 
+    public int addTotalCalories(int calories) {
+        return totalCalories += calories;
+    }
+
     @Override
-    public int compareTo(Elf arg0) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    public int compareTo(Elf elfToCompare) {
+        
+       return elfToCompare.totalCalories - totalCalories;
     }
 
 }
--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -16,21 +16,41 @@ public class TestElfShould {
 
     @Test
     void have_1000_total_calories_after_adding_1000() {
-       fail("Not yet implemented");
+       Elf elf = new Elf();
+       elf.addTotalCalories(1000);
+        assertThat(elf.getTotalCalories(), equalTo(1000));
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.addTotalCalories(100);
+
+        Elf elfHasMore = new Elf();
+        elfHasMore.addTotalCalories(1000);
+
+        assertThat(elf.compareTo(elfHasMore), greaterThan(0));
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.addTotalCalories(100);
+
+        Elf elfHasMore = new Elf();
+        elfHasMore.addTotalCalories(100);
+
+        assertThat(elf.compareTo(elfHasMore), equalTo(0));
     }
 
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.addTotalCalories(100);
+
+        Elf elfHasMore = new Elf();
+        elfHasMore.addTotalCalories(99);
+
+        assertThat(elf.compareTo(elfHasMore), lessThan(0));
     }
 }
